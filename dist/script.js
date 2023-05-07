8bad8ca3ea0f2100678c6a381d8284f0be5f9e27
resonanceee <res0nancee@pm.me>
2023-05-07 21:31:21 +0200

    First Commit!

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+skrollr.init();
+
+/* HEY THERE!
+This cave thingy is based on this pen of mine here: https://codepen.io/Ovoce/pen/jucIg/
+So go check it out... You might need it 20 years in the future.
+*/
