d24ebd766c108356df5cd96370f490fc49fabd93
resonanceee <res0nancee@pm.me>
2024-07-14 11:22:20 +0200

    🛆Remove old codebase files to prepare for vercel deployment

--- components/projects.vue
+++ components/projects.vue
@@ -2,13 +2,13 @@
     <div class="wrapper">
         <div class="title">
             <h1>Projects</h1>
-            <p>placeholder</p>
+            <p>work in progress</p>
         </div>
     </div>
 </template>
 
 <style scoped>
 .wrapper .title {
-    padding-top: 20px; /* Adjust padding as needed */
+    padding-top: 20px;
 }
 </style>
